b5f35e10daf5cd5ec48c61f58bb476b253590437
hoangdv <hoang.dv@outlook.com>
2020-10-16 09:54:12 +0900

    Add sample /64379970

---
+++ 64379970/index.js
@@ -0,0 +1,17 @@
+let express = require("express")
+let bodyParser = require("body-parser")
+let app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.listen(3000, function () {
+  console.log("working")
+})
+app.get('/', function (request, respond) {
+  respond.sendFile(__dirname + "/index.html")
+})
+app.post('/', function (req, res) {
+  let num1 = Number(req.body.num1)
+  let num2 = Number(req.body.num2)
+  let result = num1 / num2 * num2
+  res.send(result)
+})
